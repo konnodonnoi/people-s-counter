01f39521a1fe625f06e470129abc5ae7987f409d
me <saboanamero8521@gmail.com>
2022-07-31 13:09:36 +0100

    commits

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+
+const add = () => {
+  setCount (prev => count + 1)
+}
+
+const minus = () => {
+  setCount  (prev => count - 1)
+}
+
   return (
-    <div className="App">
-hi
+    <div>
+<h1>
+people entered:
+</h1>
+
+<button className='button' onClick={minus}>-</button>
+<h2>
+  {count}
+  </h2>
+  <button className='button' onClick={add}>+</button>
+  
     </div>
   );
 }
